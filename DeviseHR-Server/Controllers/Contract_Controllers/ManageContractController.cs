229f528811335314f82ea56abd633af6fda851b5
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2024-03-03 00:05:36 +0200

    created the CreateContract route and need to complete tits services and repo

---
+++ DeviseHR-Server/Controllers/Contract_Controllers/ManageContractController.cs
@@ -0,0 +1,65 @@
+﻿using DeviseHR_Server.Common;
+using DeviseHR_Server.DTOs;
+using DeviseHR_Server.Services.UserServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static DeviseHR_Server.DTOs.RequestDTOs.ManagerUserRequests;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+using DeviseHR_Server.DTOs.RequestDTOs;
+using DeviseHR_Server.Models;
+
+namespace DeviseHR_Server.Controllers.Contract_Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageContractController : ControllerBase
+    {
+
+        // Regular 3,
+        // Variable 2,
+        // Agency 1
+
+
+
+        [HttpPost("CreateContract")]
+        [Authorize(Policy = "Manager")]
+        [Authorize(Policy = "EnableAddEmployees")]
+        public async Task<ActionResult<ServiceResponse<Contract>>> CreateContract([FromBody] CreateContractRequest newConract)
+        {
+            try
+            {
+                string clientJWT = Tokens.ExtractTokenFromRequestHeaders(HttpContext);
+                Tokens.ExtractClaimsFromToken(clientJWT, false, out ClaimsPrincipal claimsPrincipal, out JwtSecurityToken jwtToken);
+
+                int myId = int.Parse(claimsPrincipal.FindFirst("id")!.Value);
+                int companyId = int.Parse(claimsPrincipal.FindFirst("companyId")!.Value);
+                int userType = int.Parse(claimsPrincipal.FindFirst("userType")!.Value);
+                DateOnly companyAnnualLeaveDate = DateOnly.Parse(claimsPrincipal.FindFirst("annualLeaveStartDate")!.Value);
+
+               // Contract addedContract = await ManagerContractService.AddContract(newConract, myId, companyId, userType, companyAnnualLeaveDate);
+               Contract addedContract = new Contract();
+                // complete this
+                // complete this
+                // complete this
+                // complete this
+                // complete this
+                // complete this
+                // complete this
+                // complete this
+
+                var serviceResponse = new ServiceResponse<Contract>(addedContract, true, "");
+
+                return Ok(serviceResponse);
+            }
+            catch (Exception ex)
+            {
+                var serviceResponse = new ServiceResponse<bool>(false, false, ex.Message);
+                return BadRequest(serviceResponse);
+            }
+        }
+
+    }
+}
---
+++ DeviseHR-Server/DTOs/RequestDTOs/ContractRequests.cs
@@ -0,0 +1,24 @@
+﻿namespace DeviseHR_Server.DTOs.RequestDTOs
+{
+       // Regular 3,
+       // Variable 2,
+       // Agency 1
+ 
+
+    public class CreateContractRequest
+    {
+        public int user_id { get; set; }
+        public int? pattern_id { get; set; }
+        public int contract_type { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime? end_date { get; set; }
+        public int contracted_working_hours_per_week_in_minutes { get; set; }
+        public int full_time_working_hours_per_week_in_minutes { get; set; }
+        public int contracted_working_days_per_week { get; set; }
+        public int avrage_working_day { get; set; }
+        public bool is_leave_in_days { get; set; }
+        public int companies_full_time_annual_leave_entitlement { get; set; }
+        public int contracted_annual_leave_entitlement { get; set; }
+        public int this_years_annual_leave_allowence { get; set; }
+        public int? term_time_id { get; set; }
+    }
