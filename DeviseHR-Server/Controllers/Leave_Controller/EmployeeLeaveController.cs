c170ea635f74fd261f3d0c951b9ba5d673070892
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2024-03-16 00:45:37 +0200

    added the request leave route structure, impliment the services and repo next

---
+++ DeviseHR-Server/Controllers/Leave_Controller/EmployeeLeaveController.cs
@@ -0,0 +1,45 @@
+﻿using DeviseHR_Server.Common;
+using DeviseHR_Server.DTOs.RequestDTOs;
+using DeviseHR_Server.DTOs;
+using DeviseHR_Server.Services.ContractServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using DeviseHR_Server.Models;
+
+namespace DeviseHR_Server.Controllers.Leave_Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveController : ControllerBase
+    {
+
+        [HttpPost("RequestLeave")]
+        [Authorize(Policy = "Employee")]
+        public async Task<ActionResult<ServiceResponse<bool>>> AddLeave([FromBody] AddAbsenceRequest newAbsence)
+        {
+            try
+            {
+                string clientJWT = Tokens.ExtractTokenFromRequestHeaders(HttpContext);
+                Tokens.ExtractClaimsFromToken(clientJWT, false, out ClaimsPrincipal claimsPrincipal, out JwtSecurityToken jwtToken);
+
+                int myId = int.Parse(claimsPrincipal.FindFirst("id")!.Value);
+                int companyId = int.Parse(claimsPrincipal.FindFirst("companyId")!.Value);
+                int userType = int.Parse(claimsPrincipal.FindFirst("userType")!.Value);
+
+              //  Contract addedContract = await ManagerContractService.AddContract(newConract, myId, companyId, userType);
+
+                var serviceResponse = new ServiceResponse<bool>(true, true, "");
+
+                return Ok(serviceResponse);
+            }
+            catch (Exception ex)
+            {
+                var serviceResponse = new ServiceResponse<Contract>(null!, false, ex.Message);
+                return BadRequest(serviceResponse);
+            }
+        }
+    }
+}
---
+++ DeviseHR-Server/DTOs/RequestDTOs/LeaveRequests.cs
@@ -0,0 +1,30 @@
+﻿using DeviseHR_Server.Models;
+
+namespace DeviseHR_Server.DTOs.RequestDTOs
+{
+    public class AddAbsenceRequest
+    {
+        public int UserId { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public bool? IsFirstHalfDay { get; set; }
+
+        public TimeOnly? StartTime { get; set; }
+
+        public TimeOnly? EndTime { get; set; }
+
+        public int AbsenceTypes { get; set; }
+
+        public string? Comments { get; set; }
+
+        public bool IsApproved { get; set; } = false;
+
+
+    }
+
+
+
+}
